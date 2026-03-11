5c79a36d7ea653fdecc341fb92764fdd4cbafbb7
UpdateBot <hoffmann.odu@gmail.com>
2026-03-11 12:47:57 +0000

    0.2.9

--- Formula/mapvas.rb
+++ Formula/mapvas.rb
@@ -3,9 +3,9 @@
 class Mapvas < Formula
   desc "mapvas - a hackable map viewer"
   homepage "https://github.com/udho/homebrew-mapvas"
-  url "https://github.com/udho/mapvas/releases/download/0.2.8/mapvas-macos.zip"
-  sha256 "63d1dc62b6e95c1a418603f6aa81f5fb2e90b1dbbf1a3143f9bb1ed9a4c2d116"
-  version "0.2.8"
+  url "https://github.com/udho/mapvas/releases/download/0.2.9/mapvas-macos.zip"
+  sha256 "b4f0309756e73d99430142e7a95672b36270603951e74edce2dd08b5f3101d0b"
+  version "0.2.9"
 
   def install
     bin.install "mapvas"
